8bb8bf555eb4475785f3c666e1f969eabb7a9fb4
paulowebphp <paulowebphp@gmail.com>
2018-11-13 23:33:59 -0200

    Carrinho de Compras - Calculo do frete

--- site.php
+++ site.php
@@ -86,7 +86,8 @@ $app->get("/cart", function()
 	$page->setTpl("cart", [
 
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 
 	]); 
 
@@ -141,6 +142,18 @@ $app->get("/cart/:idproduct/remove", function($idproduct)
 	$cart = Cart::getFromSession();
 
 	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});#END route
+
+
+$app->post("/cart/freight", function() 
+{
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
 
 	header("Location: /cart");
 	exit;
