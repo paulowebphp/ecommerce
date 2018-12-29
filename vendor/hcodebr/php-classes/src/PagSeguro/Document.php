74d18b6e451eebb92e09437ad6749884ecbbf72f
paulowebphp <paulowebphp@gmail.com>
2018-12-29 13:50:32 -0200

    Configurando classe Phone

--- vendor/hcodebr/php-classes/src/PagSeguro/Document.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Document.php
@@ -5,8 +5,82 @@ namespace Hcode\PagSeguro;
 class Document
 {
 
+	const CPF = "CPF";
+
 	private $type;
 	private $value;
+
+	public function __construct(string $type, string $value)
+	{
+
+		if( !$value )
+		{
+			throw new \Exception("Informe o numero do documento");
+			
+		}#end if
+
+		switch ($type) 
+		{
+			case Document::CPF:
+				# code...
+				if( !Document::isValidCPF($value) )
+				{
+					throw new \Exception("CPF inválido");
+					
+				}#end if
+
+				break;
+
+		}#end switch
+
+		$this->type = $type;
+		$this->value = $value;
+
+	}#END __construct
+
+
+	public static function isValidCPF($number):boolean
+	{
+
+		$number = preg_replace('/[^0-9]/', '', (string) $number);
+
+	    if (strlen($number) != 11)
+	        return false;
+
+	    for ($i = 0, $j = 10, $sum = 0; $i < 9; $i++, $j--)
+	        $sum += $number{$i} * $j;
+	    $rest = $sum % 11;
+	    if ($number{9} != ($rest < 2 ? 0 : 11 - $rest))
+	        return false;
+
+	    for ($i = 0, $j = 11, $sum = 0; $i < 10; $i++, $j--)
+	        $sum += $number{$i} * $j;
+	    $rest = $sum % 11;
+
+	    return ($number{10} == ($rest < 2 ? 0 : 11 - $rest));
+
+	}#END isValidCPF
+
+
+	public function getDOMElement():DOMElement
+	{
+
+		$dom = new DOMDocument();
+
+		$document = $dom->createElement("document");
+		$document = $dom->appendChild($document);
+
+		$type = $dom->createElement("type", $this->type);
+		$type = $document->appendChild($type);
+
+		$value = $dom->createElement("value", $this->value);
+		$value = $document->appendChild($value);
+
+		return $document;
+
+	}#END getDOMElement
+
+
 
 
 }#END class Document
--- vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Phone.php
@@ -9,6 +9,64 @@ class Phone
 	private $number;
 
 
+	public function __construct(int $areaCode, int $number)
+	{
+
+		# Validando DDD
+		if(
+			!$areaCode
+			||
+			$areaCode < 11
+			||
+			$areaCode > 99
+		)
+
+		{
+			throw new \Exception("Informe o DDD");
+			
+		}#end if
+
+
+		# Validando ó número do telefone
+		if(
+			!$number
+			||
+			strlen($number) < 8
+			||
+			strlen($number) > 9
+		)
+			
+		{
+			throw new \Exception("Informe o número do telefone");
+			
+		}#end if
+
+
+		$this->areaCode = $areaCode;
+		$this->number = $number;
+
+	}#END __construct	
+
+
+	public function getDOMElement():DOMElement
+	{
+
+		$dom = new DOMDocument();
+
+		$phone = $dom->createElement("phone");
+		$phone = $dom->appendChild($phone);
+
+		$areaCode = $dom->createElement("areaCode", $this->areaCode);
+		$areaCode = $phone->appendChild($areaCode);
+
+		$number = $dom->createElement("number", $this->number);
+		$number = $phone->appendChild($number);
+
+		return $phone;
+
+	}#END getDOMElement
+
+
 }#END class Phone
 
 
